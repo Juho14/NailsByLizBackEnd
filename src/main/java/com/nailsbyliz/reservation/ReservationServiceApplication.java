ff01d9eae97d708388d7b69ce490f650e4ff8942
Juho14 <juqiqekli@gmail.com>
2024-03-24 21:12:51 +0200

    Altering properties to fix deployment.

--- src/main/java/com/nailsbyliz/reservation/ReservationServiceApplication.java
+++ src/main/java/com/nailsbyliz/reservation/ReservationServiceApplication.java
@@ -1,14 +1,13 @@
 package com.nailsbyliz.reservation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(ReservationServiceApplication.class);
+	// private static final Logger log =
+	// LoggerFactory.getLogger(ReservationServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
